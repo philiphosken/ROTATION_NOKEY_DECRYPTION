3fb3132d50dd71d10d16f846de4aea86c644a22f
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-10 12:42:37 +0000

    Final commit...All further under "c3331534_Assessment3"

--- Decryption_NoKey_Rotation.c
+++ Decryption_NoKey_Rotation.c
@@ -2,9 +2,8 @@
 
 int main()
 {
-
-char N = 27;
 unsigned char str[1024];
+char k = 1;
 
 FILE *in;
 in = fopen("in.txt", "r");
@@ -13,37 +12,5 @@ FILE *out;
 out = fopen("out.txt", "w");
     
     while (feof(in) == 0){
-        fscanf(in, "%s", str);
-        printf("%s", str);
-        }
-        
-        for (k = 0 ; k < 26 ; k++){
-            
+        fscanf(in, "%s\n", str);
 
-        }
-        do k = 0
-        if not a word add k
-        match yes 
-        no add k
-        //if str matches dictionary --> print result --> else + k
-        //if str + k matches dictioonary --> print reselt --> else + k
-        
-      
-        }
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-    }
-
-
-   
