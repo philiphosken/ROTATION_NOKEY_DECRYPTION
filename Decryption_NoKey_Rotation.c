8462b08a70452f68070cc80df4c75190bd990747
Philip Hosken <philip.hosken@uon.edu.au>
2019-04-10 09:07:48 +0000

    Initial Code for no key decryption

--- Decryption_NoKey_Rotation.c
+++ Decryption_NoKey_Rotation.c
@@ -1,3 +1,39 @@
 #include <stdio.h>
 
 int main()
+{
+char alphatxt[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ";
+
+/* char subtxt[] = unknown */
+
+char N = 27;
+unsigned char str[1024];
+
+FILE *in;
+in = fopen("in.txt", "r");
+
+FILE *out;
+out = fopen("out.txt", "w");
+    
+    while (feof(in) == 0){
+        fscanf(in, "%s", str);
+        printf("%s", str);
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+   
